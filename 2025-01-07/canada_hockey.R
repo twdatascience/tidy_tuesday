982b0eb11b19863d07e5f7e3200f1fbc262c925e
Tyler Wilson <twtylerwilson@gmail.com>
2025-01-09 17:08:42 -0700

    made map

--- 2025-01-07/canada_hockey.R
+++ 2025-01-07/canada_hockey.R
@@ -3,6 +3,9 @@ library(tidytuesdayR)
 library(dplyr)
 library(ggplot2)
 library(gganimate)
+library(maps)
+library(sf)
+library(stringr)
 
 tuesdata <- tidytuesdayR::tt_load('2024-01-09')
 
@@ -49,7 +52,7 @@ hold <- canada_births_1991_2022 |>
   labs(title = "Year: {closest_state}") +
   transition_states(year)
 
-animate(hold, res = 150, fps = 20, width = 10, height = 8, units = "in")
+animate(hold, res = 150)
 
 
 
@@ -58,3 +61,80 @@ canada_births_1991_2022 |>
   summarise(total_births = sum(births)) |> 
   ggplot(aes(x = year, y = total_births)) +
   geom_line(group = 1)
+
+# look at nhl_player_births
+nhl_player_births |> 
+  filter(birth_year > 1990) |>
+  filter(birth_country %in% c("CAN", "USA")) |> 
+  ggplot(aes(x = birth_year, fill = birth_country)) +
+  geom_histogram(binwidth = 1, position = "dodge")
+
+nhl_player_births |> 
+  filter(birth_year > 1990) |>
+  group_by(birth_country, birth_state_province) |>
+  summarise(total_players = n()) |> 
+  arrange(desc(total_players)) |> 
+  ungroup() |> 
+  filter(total_players > 10)
+
+nhl_player_births |> 
+  group_by(birth_city) |> 
+  summarise(total_players = n()) |> 
+  arrange(desc(total_players))
+
+
+
+# just canada
+nhl_can_cities <- nhl_player_births |> 
+  filter(birth_country == "CAN") |>
+  pull(birth_city) |>
+  unique()
+  
+  
+canada_map <- map_data("world", region = "Canada")
+
+canada_map |> 
+  ggplot(aes(x = long, y = lat, group = group)) +
+  geom_polygon(fill = "lightblue", color = "white") +
+  coord_fixed(1.3)
+
+canada_provinces <- st_read("https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/ne_10m_admin_1_states_provinces.geojson") |> 
+  filter(iso_a2 == "CA")
+
+canada_provinces |>
+  ggplot() +
+  geom_sf() +
+  coord_sf()
+
+# only has cities in canada.cities
+city_locs <- canada.cities |> 
+    mutate(city = substr(name, 1, nchar(name) - 3),
+           province = str_extract(name, "[^ ]+$")) |>
+  filter(city %in% nhl_can_cities)
+
+can_city_count <- nhl_player_births |> 
+  filter(birth_city %in% city_locs$city) |>
+  group_by(birth_city) |>
+  summarise(total_players = n())
+
+city_locs <- city_locs |>
+  left_join(can_city_count, by = c("city" = "birth_city"))
+
+canada_provinces |> 
+  ggplot() +
+  geom_sf() +
+  geom_point(data = city_locs, 
+             aes(x = long, y = lat, size = total_players), 
+             shape = 21,
+             fill = "red",
+             stroke = 0.5,
+             alpha = 0.3) +
+  coord_sf() +
+  labs(title = "NHL Player Birth Cities in Canada",
+       size = "Total Players Born in City") +
+  theme_minimal() +
+  theme(panel.grid = element_blank(),
+        axis.text = element_blank(),
+        axis.title = element_blank(),
+        axis.ticks = element_blank(),
+        legend.position = "bottom")
